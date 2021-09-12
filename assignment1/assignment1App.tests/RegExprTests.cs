91a595fca91e2ff9cad0f21cde50a8340fff54de
tokj <tokj@itu.dk>
2021-09-12 23:19:40 +0200

    almost done with RegEx

--- assignment1/assignment1App.tests/RegExprTests.cs
+++ assignment1/assignment1App.tests/RegExprTests.cs
@@ -1,8 +1,88 @@
 using Xunit;
+using Assignment1;
+using System.Collections.Generic;
+using System;
 
 namespace Assignment1.Tests
 {
     public class RegExprTests
     {
+        [Fact]
+        public void SplitLine_given_empty_list()
+        {
+        //Given
+        var input = new List<string>{};
+        var expected = new List<string>{};
+        //When
+        var actual = RegExpr.SplitLine(input);
+        //Then
+        Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void SplitLine_given_non_empty_list_returns()
+        {
+        //Given
+        var input = new List<string>{"this is a test", "this;is;a;test"};
+        var expected = new List<string>{"this","is","a","test","this","is","a","test"};
+        //when
+        var actual = RegExpr.SplitLine(input);
+        //Then
+        Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void Resolution_given_empty_list()
+        {
+        //Given
+        var input = "";
+        var expected = new List<(int,int)>();
+        //When
+        var actual = RegExpr.Resolution(input);
+        //Then
+        Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void Resolution_given_list_with_resolutions()
+        {
+        //Given
+        var input = "1920x1080 1024x768, 800x600, 640x480 320x200, 320x240, 800x600 1280x960";
+        var expected = new List<(int,int)>{
+            (1920, 1080),
+            (1024, 768),
+            (800, 600),
+            (640, 480),
+            (320, 200),
+            (320, 240),
+            (800, 600),
+            (1280, 960)
+        };
+        //When
+        var actual = RegExpr.Resolution(input);
+        //Then
+        Assert.Equal(expected,actual);
+        }
+
+        [Fact]
+        public void Resolution_given_list_with_resolutions_and_words()
+        {
+        //Given
+        var input = "1920x1080 1024x768, this 800x600, 640x480 is320x200, 320x240,  not 800x600 supposed to be here 1280x960";
+        var expected = new List<(int,int)>{
+            (1920, 1080),
+            (1024, 768),
+            (800, 600),
+            (640, 480),
+            (320, 200),
+            (320, 240),
+            (800, 600),
+            (1280, 960)
+        };
+        //When
+        var actual = RegExpr.Resolution(input);
+        //Then
+        Assert.Equal(expected,actual);
+        }
     }
 }
--- assignment1/assignment1App/RegExpr.cs
+++ assignment1/assignment1App/RegExpr.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Text.RegularExpressions;
 
 namespace Assignment1
 {
@@ -7,12 +8,29 @@ namespace Assignment1
     {
         public static IEnumerable<string> SplitLine(IEnumerable<string> lines)
         {
-            throw new NotImplementedException();
+            var pattern = @"(\w+)";
+            var rx = new Regex(pattern);
+            foreach (var line in lines)
+            {
+                var match = rx.Match(line);
+                while(match.Success){
+                    yield return match.Value;
+                    match = match.NextMatch();
+                }
+            }
         }
 
         public static IEnumerable<(int width, int height)> Resolution(string resolutions)
         {
-            throw new NotImplementedException();
+            var pattern = @"(?<width>\d+)x(?<height>\d+)";
+            var rx = new Regex(pattern);
+            var match = rx.Match(resolutions);
+            while(match.Success){
+                var width = int.Parse(match.Groups["width"].Value);
+                var height = int.Parse(match.Groups["height"].Value);
+                yield return (width,height);
+                match = match.NextMatch();
+            }
         }
 
         public static IEnumerable<string> InnerText(string html, string tag)
